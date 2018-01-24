63628841b74095a1444b9d1bf2a1d7fece57d7c4
Jorge <jorge.costa@developer.pt>
2018-01-24 12:33:42 +0000

    Added ability to set page template.

---
+++ editor/components/page-attributes/template.js
@@ -0,0 +1,66 @@
+/**
+ * External dependencies
+ */
+import { connect } from 'react-redux';
+import { map } from 'lodash';
+
+/**
+ * WordPress dependencies
+ */
+import { __ } from '@wordpress/i18n';
+import { withContext, withInstanceId } from '@wordpress/components';
+import { compose } from '@wordpress/element';
+
+/**
+ * Internal dependencies
+ */
+import './style.scss';
+import { getEditedPostAttribute } from '../../store/selectors';
+import { editPost } from '../../store/actions';
+
+export function PageTemplate( { availableTemplates, selectedTemplate, instanceId, onUpdateTemplate } ) {
+	const selectId = `template-selector-${ instanceId }`;
+
+	// Disable reason: A select with an onchange throws a warning
+
+	/* eslint-disable jsx-a11y/no-onchange */
+	return (
+		<div className="editor-page-attributes__template">
+			<label htmlFor={ selectId }>{ __( 'Template:' ) }</label>
+			<select
+				id={ selectId }
+				value={ selectedTemplate }
+				onChange={ ( event ) => onUpdateTemplate( event.target.value ) }
+			>
+				{ map( { '': __( 'Default template' ), ...availableTemplates }, ( templateName, templateSlug ) => (
+					<option key={ templateSlug || 'default' } value={ templateSlug }>{ templateName }</option>
+				) ) }
+			</select>
+		</div>
+	);
+}
+
+const applyConnect = connect(
+	( state ) => {
+		return {
+			selectedTemplate: getEditedPostAttribute( state, 'template' ),
+		};
+	},
+	{
+		onUpdateTemplate( templateSlug ) {
+			return editPost( { template: templateSlug || '' } );
+		},
+	}
+);
+
+const applyWithContext = withContext( 'editor' )(
+	( settings ) => ( {
+		availableTemplates: settings.availableTemplates,
+	} )
+);
+
+export default compose( [
+	applyConnect,
+	applyWithContext,
+	withInstanceId,
+] )( PageTemplate );
