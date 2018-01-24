d323be6eb94acffbde8e47e1ce8c950660411efc
Jorge <jorge.costa@developer.pt>
2018-01-24 15:43:32 +0000

    Used correct attribute label, instead of "Page Attributes" for all post types.

--- editor/edit-post/sidebar/page-attributes/index.js
+++ editor/edit-post/sidebar/page-attributes/index.js
@@ -2,30 +2,35 @@
  * External dependencies
  */
 import { connect } from 'react-redux';
+import { get } from 'lodash';
 
 /**
  * WordPress dependencies
  */
 import { __ } from '@wordpress/i18n';
-import { PanelBody, PanelRow } from '@wordpress/components';
+import { PanelBody, PanelRow, withAPIData } from '@wordpress/components';
+import { compose } from '@wordpress/element';
 
 /**
  * Internal dependencies
  */
 import { PageAttributesCheck, PageAttributesOrder, PageAttributesParent, PageTemplate } from '../../../components';
 import { toggleSidebarPanel } from '../../../store/actions';
-import { isEditorSidebarPanelOpened } from '../../../store/selectors';
+import { getCurrentPostType, isEditorSidebarPanelOpened } from '../../../store/selectors';
 
 /**
  * Module Constants
  */
 const PANEL_NAME = 'page-attributes';
 
-export function PageAttributes( { isOpened, onTogglePanel } ) {
+export function PageAttributes( { isOpened, onTogglePanel, postType } ) {
+	if ( ! postType.data ) {
+		return null;
+	}
 	return (
 		<PageAttributesCheck>
 			<PanelBody
-				title={ __( 'Page Attributes' ) }
+				title={ get( postType, 'data.labels.attributes', __( 'Page Attributes' ) ) }
 				opened={ isOpened }
 				onToggle={ onTogglePanel }
 			>
@@ -39,10 +44,11 @@ export function PageAttributes( { isOpened, onTogglePanel } ) {
 	);
 }
 
-export default connect(
+const applyConnect = connect(
 	( state ) => {
 		return {
 			isOpened: isEditorSidebarPanelOpened( state, PANEL_NAME ),
+			postTypeSlug: getCurrentPostType( state ),
 		};
 	},
 	{
@@ -50,4 +56,16 @@ export default connect(
 			return toggleSidebarPanel( PANEL_NAME );
 		},
 	}
-)( PageAttributes );
+);
+
+const applyWithAPIData = withAPIData( ( props ) => {
+	const { postTypeSlug } = props;
+	return {
+		postType: `/wp/v2/types/${ postTypeSlug }?context=edit`,
+	};
+} );
+
+export default compose( [
+	applyConnect,
+	applyWithAPIData,
+] )( PageAttributes );
